edb135818df1fb0764fc6fdae7d3d38a1ad86efa
paul <paul@b7interactive.com>
2015-08-16 14:38:10 -0600

    full-screen, change marker icon, added marker click function to show note

--- js/controllers/main.js
+++ js/controllers/main.js
@@ -1,131 +1,89 @@
 angular.module('checkinController', [])
-	.controller('mainController', ['$scope','$http','Checkins','Traildata','uiGmapGoogleMapApi','uiGmapIsReady', function($scope, $http, Checkins, Traildata, uiGmapGoogleMapApi, uiGmapIsReady) {
-		$scope.checkinData = {};
+	.controller('mainController', ['$scope','$http','Checkins','Traildata','uiGmapGoogleMapApi','$q', function($scope, $http, Checkins, Traildata, uiGmapGoogleMapApi, $q) {
 		$scope.loading = true;
 
-		$scope.options = {
-			scrollwheel: false
-		}
+		// chained promises: google maps UI > trail data > checkin data
+		uiGmapGoogleMapApi.then(function(maps) {
+			return Traildata.get();
+		}).then(function(trailData) {
+			$scope.trailData = trailData.data;
+			return Checkins.get();
+		}).then(function(checkins) {
+			$scope.checkins = checkins.data;
 
-		Checkins.get()
-			.success(function(data) {
-				$scope.checkins = data;
-
-
-				var createCheckinMarker = function(i, idKey) {
-					if (idKey == null) {
-						idKey = "id";
+			var createCheckinMarker = function(i) {
+				var marker = {
+					latitude: $scope.checkins[i].lat,
+					longitude: $scope.checkins[i].lon,
+					showWindow: false,
+					note: $scope.checkins[i].text,
+					id: i,
+					optimized: false,
+					options: {
+						labelContent: ' ',
+						labelAnchor: "-15 -15",
+						labelClass: "marker-labels"
+					},
+					icon: {
+						url: "img/bike.png",
+						scaledSize: new google.maps.Size(40, 40), // scaled size
+						origin: new google.maps.Point(0,0), // origin
+						anchor: new google.maps.Point(20, 20) // anchor
 					}
-
-					var latitude = $scope.checkins[i].lat;
-					var longitude = $scope.checkins[i].lon;
-					var title = 'checkin' + i;
-
-					var returned = {
-						latitude: latitude,
-						longitude: longitude,
-						title: title
-					}
-					returned[idKey] = i;
-					return returned;
 				}
+				return marker;
+			}
 
-			    uiGmapGoogleMapApi.then(function(maps) {
-					Traildata.get()
-						.success(function(trailData) {
-							$scope.trailData = trailData;
+			$scope.mapOptions = {
+				panControl    : true,
+				zoomControl   : true,
+				scaleControl  : true,
+				mapTypeControl: true,
+				streetViewControl: false,
+				mapTypeId     : google.maps.MapTypeId.TERRAIN
+			};
 
-							$scope.polylines = [
-							    {
-							        id: 1,
-							        path: $scope.trailData.features[0].geometry,
-							        stroke: {
-							            color: '#6060FB',
-							            weight: 3
-							        },
-							        editable: false,
-							        draggable: false,
-							        geodesic: true,
-							        visible: true
-							    }
-							];
+			// init coTrail line
+			$scope.polylines = [{
+				id: 1,
+				path: $scope.trailData.features[0].geometry,
+				stroke: {
+					color: '#6060FB',
+					weight: 3
+				}
+			}];
 
-							$scope.map = {
-								center: { latitude: $scope.checkins[0].lat, longitude: $scope.checkins[0].lon },
-								zoom: 8,
-								options: $scope.MapOptions
-							};
+			// init checkinMarkers
+			$scope.checkinMarkers = [];
+			var markers = [];
+			for (var i = 0; i < $scope.checkins.length; i++) {
+				markers.push(createCheckinMarker(i));
+			}
+			var newestMarker = markers[markers.length - 1];
+			$scope.checkinMarkers = markers;
 
-							$scope.checkinMarkers = [];
+			// init map
+			$scope.map = {
+				options: $scope.mapOptions,
+				center: { latitude: newestMarker.latitude, longitude: newestMarker.longitude },
+				zoom: 10,
+				markersEvents: {
+					click: function (marker, eventName, model, args) {
+						model.options.labelContent = "lat: " + model.latitude + " lon: " + model.longitude;
+						model.showWindow = true;
+						$scope.$apply();
+					},
+					mouseover: function (marker, eventName, model, args) {
+						model.options.labelContent = "lat: " + model.latitude + " lon: " + model.longitude;
+						$scope.$apply();
+					},
+					mouseout: function (marker, eventName, model, args) {
+						model.options.labelContent = " ";
+						$scope.$apply();
+					}
+				}
+			};
 
-							var markers = [];
-							for (var i = 0; i < $scope.checkins.length; i++) {
-								markers.push(createCheckinMarker(i));
-							}
-
-							$scope.checkinMarkers = markers;
-
-//							$scope.addMarkerClickFunction($scope.checkinMarkers);
-
-							$scope.loading = false;
-
-
-						});
-			    });
-			});
-
-		$scope.windowOptions = {
-		    show: true
-		};
-/*
-		$scope.onClick = function (data) {
-		    $scope.windowOptions.show = !$scope.windowOptions.show;
-		    console.log('$scope.windowOptions.show: ', $scope.windowOptions.show);
-		    console.log('This is a ' + data);
-		};
-
-		$scope.closeClick = function () {
-		    $scope.windowOptions.show = false;
-		};
-
-		$scope.title = "Window Title!";
-
-		$scope.addMarkerClickFunction = function (markersArray) {
-		    angular.forEach(markersArray, function (value, key) {
-		        value.onClick = function () {
-		            $scope.onClick(value.data);
-		            $scope.MapOptions.markers.selected = value;
-		        };
-		    });
-		};
-*/
-
-		$scope.MapOptions = {
-		    minZoom: 3,
-		    zoomControl: false,
-		    draggable: true,
-		    navigationControl: false,
-		    mapTypeControl: false,
-		    scaleControl: false,
-		    streetViewControl: false,
-		    disableDoubleClickZoom: false,
-		    keyboardShortcuts: true,
-		    markers: {
-		        selected: {}
-		    },
-		    styles: [{
-		        featureType: "poi",
-		        elementType: "labels",
-		        stylers: [{
-		            visibility: "off"
-		        }]
-		    }, {
-		        featureType: "transit",
-		        elementType: "all",
-		        stylers: [{
-		            visibility: "off"
-		        }]
-		    }],
-		};
-
+			$scope.loading = false;
+		});
 	}]);
